af94a89b2eb4fb940818b421d113b356bce424b8
GitHub Action <action@github.com>
2025-11-14 09:04:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/krisner94/micronautplugin/MyBundle.kt
+++ src/main/kotlin/com/github/krisner94/micronautplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.krisner94.micronautplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/krisner94/micronautplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/krisner94/micronautplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.krisner94.micronautplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
